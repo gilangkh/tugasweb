c08ce44026b7d82b1904b90fa7f14a95e80419a4
Gilang2812 <gilanggkh@gmail.com>
2023-04-08 12:14:14 +0700

    user

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var app = express()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-
 });
 
-
-app.listen(8000, function(){
-  console.log("port 30000")
-})
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,109 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../modules/db')
+var db = require('../modules/db');
+var User = require('../models/users');
+const { response } = require('express');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  
+  //Koneksi ke database
+  let connection = db.connection;
+  
+  let sql = "SELECT id, username, email, active FROM users";
+  connection.query(sql, (err, rows, fields) => {
+    if(err) throw err;
+    
+    res.json(rows);
+  });
+  
 });
 
-module.exports = router;
+/* TAMBAH USERS */
+router.post('/', async (req, res, next) => {
+  //1. Buat koneksi ke database
+  // let connection = db.connection;
+  
+  //2. Ambil data yang akan ditambahkan
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let active = req.body.active;
+  let sign_img = req.body.sign_img;
+  
+  //3. Tambahkan data ke dalam database
+  await User.create({
+    username: username,
+    email: email,
+    password: password,
+    active: active,
+    sign_img: sign_img
+  
+  }).then((res) => {
+    let response = {
+      message: "Data berhasil ditambahkan",
+    };
+    
+    res.json(response);
+  }).catch((err) => {
+    console.log(err);
+  })
 
+  });
+  
+  /* EDIT USERS */
+  router.post('/:id/edit', (req, res, next) => {
+    //1. Koneksi ke databaes
+    let connection = db.connection;
+    
+    //2. Ambil id data yang akan diedit
+    let id = req.params.id;
+    
+    //3. Ambil data update
+    let name = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password;
+    let sign_img = req.body.sign_img;
+    let active = req.body.active;
+    
+    //4. Update data di database
+    let sql = "UPDATE users SET name=?, email=?, password=?, avatar=?, active=?, updated_at = now() WHERE id=?";
+    connection.query(
+      sql, 
+      [name, email, password, active,sign_img, id], 
+      (err, rows, fields) => {
+        if(err) throw err;
+        
+        let response = {
+          message: "Data berhasil diupdate",
+          affectedRows: rows.affectedRows
+        };
+        
+        res.json(response);
+      });
+    });
+    
+    /* DELETE USERS */
+    router.post('/:id/delete', (req, res, next) => {
+      //1. Koneksi ke database
+      let connection = db.connection;
+      
+      //2. Ambil ID data yang akan dihapus (M DZAKY)
+      let id = req.params.id;
+      
+      //3. Hapus data dari database
+      let sql = 'DELETE FROM users WHERE id=?';
+      connection.query(sql, [id], (err, rows, fields) => {
+        if(err) throw err;
+        
+        let response = {
+          message: "Data berhasil dihapus",
+          affectedRows: rows.affectedRows
+        };
+        
+        res.json(response);
+      });
+    });
+    
+    module.exports = router;
+    
