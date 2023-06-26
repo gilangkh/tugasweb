4d03147c1779782158f09f4e6f3bf2c3602709b2
Gilang2812 <gilanggkh@gmail.com>
2023-06-26 12:33:24 +0700

    asd

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import App from './Apps';
-import Login from './components/Login';
-
-const port = 8080; // Ganti dengan port yang Anda inginkan
+const React = require('react')
+const ReactDOM = require('react-dom')
 
 ReactDOM.render(
   <h1>TEST HOSTING</h1>,
   document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <div id="root">
-//       <Login />
-//     </div>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
