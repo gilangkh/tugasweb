bcb3a4cfa2873fd20bbec1fbc0017723de2a61e1
Gilang2812 <gilanggkh@gmail.com>
2023-06-28 14:18:07 +0700

    asd

--- public/index.js
+++ public/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Gilang() {
-  return <h1>Hello Gilang</h1>;
-}
 
-ReactDOM.render(<Gilang />, document.getElementById("root"));
+
+ReactDOM.render (<h1>Hello Gilang</h1>, document.getElementById("root"));
